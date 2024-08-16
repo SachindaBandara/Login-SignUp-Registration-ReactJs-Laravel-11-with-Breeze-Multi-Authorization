aecd32aaa5479b0838304eb128df1826f1a0680a
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-16 21:24:45 +0530

    add

--- login-signup-registration/react/src/router.jsx
+++ login-signup-registration/react/src/router.jsx
@@ -1,17 +1,40 @@
 import { createBrowserRouter } from "react-router-dom";
+import Login from "./views/login";
+import SignUp from "./views/SignUp";
+import User from "./views/User";
+import NotFound from "./views/NotFound";
+import DefaultLayout from "./components/Default";
+import Guestlayout from "./components/Guestlayout";
 
 const router = createBrowserRouter([
   {
-    path: "/Login",
-    element: <Login />,
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/User",
+        element: <User />,
+      },
+    ],
   },
   {
-    path: "/SignUp",
-    element: <SignUp />,
+    path: "/",
+    element: <Guestlayout />,
+    children: [
+      {
+        path: "/Login",
+        element: <Login />,
+      },
+      {
+        path: "/SignUp",
+        element: <SignUp />,
+      },
+    ],
   },
+
   {
-    path: "/User",
-    element: <User />,
+    path: "*",
+    element: <NotFound />,
   },
 ]);
 
