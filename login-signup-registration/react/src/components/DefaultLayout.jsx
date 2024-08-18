56249efb7932ee6955bc4ab71be38c6a04a86bab
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-08-18 16:25:05 +0530

    add

--- login-signup-registration/react/src/components/DefaultLayout.jsx
+++ login-signup-registration/react/src/components/DefaultLayout.jsx
@@ -10,21 +10,19 @@ export default function DefaultLayout() {
   }
 
   return (
-    <div>
-      <div id="defaultLayout">
-        <aside>
-          <Link to="/dashboard">DashBoard</Link>
-          <Link to="/user">User</Link>
-        </aside>
-        <div className="content">
-          <header>
-            <div>Header</div>
-            <div>User Info</div>
-          </header>
-          <main>
-            <Outlet />
-          </main>
-        </div>
+    <div id="defaultLayout">
+      <aside>
+        <Link to="/dashboard">DashBoard</Link>
+        <Link to="/user">User</Link>
+      </aside>
+      <div className="content">
+        <header>
+          <div>Header</div>
+          <div>User Info</div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
       </div>
     </div>
   );
