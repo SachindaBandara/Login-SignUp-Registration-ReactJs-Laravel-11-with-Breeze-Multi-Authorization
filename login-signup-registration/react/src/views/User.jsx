090ce2e6888c0046f67a953a0d6fcbeed6e871bd
Sachinda Bandara <118462732+SachindaBandara@users.noreply.github.com>
2024-08-19 14:07:18 +0530

    add

--- login-signup-registration/react/src/views/User.jsx
+++ login-signup-registration/react/src/views/User.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 
 export default function User() {
   return (
-<<<<<<< HEAD
-    <div> User</div>
-=======
-    <div>reUser</div>
->>>>>>> ee8d65d7d230871bcd4b71b0d72fbc0380befb3a
+    <div>re     User</div>
   )
 }
